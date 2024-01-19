faf96ec3981dcfb02d3417c1d615379d039dcba8
“bhavadharani16” <bhavadharaniep10@gmail.com>
2024-01-19 07:38:57 +0000

    First source code commit

---
+++ hello.java
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<p>Click Here</p>
+
+<img src="https://probucket1604.s3.amazonaws.com/lord-krishna-best-hd-wallpaper-for-laptop-1080p.jpg">
+
+<button onclick="newfunction()">Click to begin</button>
+
+<script>
+function newfunction()
+{
+window.open("http://App-LB-581863958.eu-west-3.elb.amazonaws.com/A-Dynamic-E-Commerce-Website/mangola/");
+}
+</script>
+
+</body>
+</html>
